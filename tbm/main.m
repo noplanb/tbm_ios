49cf450b646e647fdbe848d04f45309954a4e034
Sani Elfishawy <sani@sbcglobal.net>
2014-04-24 12:46:21 -0700

    Initial Commit

---
+++ tbm/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tbm
+//
+//  Created by Sani Elfishawy on 4/24/14.
+//  Copyright (c) 2014 No Plan B. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TBMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TBMAppDelegate class]));
+    }
+}
