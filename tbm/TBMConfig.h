0894de88d3aa56b7d83c2d56f40c9cb1e5cf8ca7
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-03-03 14:30:40 -0800

    Icons and config

--- tbm/TBMConfig.h
+++ tbm/TBMConfig.h
@@ -11,14 +11,12 @@ static NSString *CONFIG_APP_NAME = @"Zazo";
 
 // Make sure these 4 are set correctly before a release.
 static NSString *CONFIG_VERSION_NUMBER = @"18";
-static NSString *CONFIG_VERSION_STRING = @"1.426";
+static NSString *CONFIG_VERSION_STRING = @"2.0.0";
 
 // This is necessary because fucking apple has a different apns gateway depending on whether the device is
 // provisioned with a dev cert or production/adhoc cert
 static NSString *CONFIG_DEVICE_BUILD = @"prod"; // prod / dev
 
-//static const NSString *CONFIG_SERVER_BASE_URL_STRING = @"http://www.threebyme.com";
-//static const NSString *CONFIG_SERVER_BASE_URL_STRING = @"http://zazo-dev1-5.elasticbeanstalk.com";
 //static NSString *CONFIG_SERVER_BASE_URL_STRING = @"http://192.168.1.82:3000";
 static NSString *CONFIG_SERVER_BASE_URL_STRING = @"http://prod.zazoapp.com";
 
