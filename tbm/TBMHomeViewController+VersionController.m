9bba616286534f73db9cadddfa6c6137f3debbb6
Sani Elfishawy <elfishawy.sani@gmail.com>
2015-05-20 19:30:16 -0700

    Version handler bug fix
    Sends to correct url for app store.
    Mandatory upgrade does not provide a cancel button on the alert.

--- tbm/TBMHomeViewController+VersionController.m
+++ tbm/TBMHomeViewController+VersionController.m
@@ -33,9 +33,11 @@
 
 - (void)showVersionHandlerDialogWithMessage:(NSString *)message negativeButton:(BOOL)negativeButton{
     TBMAlertController *alert = [TBMAlertController alertControllerWithTitle:@"Update Available" message:message];
-    [alert addAction:[SDCAlertAction actionWithTitle:@"Later" style:SDCAlertActionStyleCancel handler:nil]];
+    if (negativeButton)
+        [alert addAction:[SDCAlertAction actionWithTitle:@"Later" style:SDCAlertActionStyleCancel handler:nil]];
+    
     [alert addAction:[SDCAlertAction actionWithTitle:@"Update" style:SDCAlertActionStyleDefault handler:^(SDCAlertAction *action) {
-        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://testflightapp.com"]];
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:CONFIG_APP_STORE_URL]];
     }]];
     [alert presentWithCompletion:nil];
 }
