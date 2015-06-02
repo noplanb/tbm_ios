a2188e9d09a8dbb39d4a3421347b268362c67308
Maksim Bazarov <bazaroffma@gmail.com>
2015-06-02 19:08:18 +0300

    258: Keyboards restricted only for English and non-standard keyboard

--- tbm/TBMRegisterForm.m
+++ tbm/TBMRegisterForm.m
@@ -200,7 +200,7 @@ static const float TBMRegisterTextFieldWidthMultiplier = 0.1;
 
     self.firstName = [[TBMTextField alloc] initWithFrame:f];
     self.firstName.placeholder = @"First Name";
-    [self.firstName setKeyboardType:UIKeyboardTypeAlphabet];
+    [self.firstName setKeyboardType:UIKeyboardTypeDefault];
     [self.firstName setReturnKeyType:UIReturnKeyNext];
     [self setCommonAttributesForTextField:self.firstName];
     [self.contentView addSubview:self.firstName];
@@ -215,7 +215,7 @@ static const float TBMRegisterTextFieldWidthMultiplier = 0.1;
 
     self.lastName = [[TBMTextField alloc] initWithFrame:f];
     self.lastName.placeholder = @"Last Name";
-    [self.lastName setKeyboardType:UIKeyboardTypeAlphabet];
+    [self.lastName setKeyboardType:UIKeyboardTypeDefault];
     [self.lastName setReturnKeyType:UIReturnKeyNext];
     [self setCommonAttributesForTextField:self.lastName];
     [self.contentView addSubview:self.lastName];
