81415435a855275fa40e084e8383f9b028ac8743
Maksim Bazarov <bazaroffma@gmail.com>
2015-08-21 11:21:50 +0300

    All features boilerplate

---
+++ Modules/Features/Delete Friend Feature/TBMDeleteFriendFeatureView.m
@@ -0,0 +1,16 @@
+//
+// Created by Maksim Bazarov on 15/06/15.
+// Copyright (c) 2015 No Plan B. All rights reserved.
+//
+
+#import "TBMDeleteFriendFeatureView.h"
+
+@implementation TBMDeleteFriendFeatureView {
+
+}
+
+- (void)configureFeatureDialog {
+    self.featureDescription = @"Abort record!";
+}
+
+@end
