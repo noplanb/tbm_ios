ae10c82f75fb55429262efb89a4650e180043db3
oks <oksana@anoda.mobi>
2015-08-26 03:26:50 +0300

    add new methods and safety for key

--- Project/Zazo/Vendor/ANODA/ANCategories/NSObject/NSObject+ANUserDefaults.h
+++ Project/Zazo/Vendor/ANODA/ANCategories/NSObject/NSObject+ANUserDefaults.h
@@ -8,8 +8,19 @@
 
 @interface NSObject (ANUserDefaults)
 
-- (NSUserDefaults *)an_dataSource;
+
+#pragma mark - Update Objects
+
 - (void)an_updateObject:(id)object forKey:(NSString*)key;
+- (void)an_updateBool:(BOOL)object forKey:(NSString*)key;
+- (void)an_updateInteger:(NSInteger)object forKey:(NSString*)key;
+
+
+#pragma mark - Retrive Objects
+
+- (id)an_objectForKey:(NSString*)key;
 - (NSString*)an_stringForKey:(NSString*)key;
+- (BOOL)an_boolForKey:(NSString*)key;
+- (NSInteger)an_integerForKey:(NSString*)key;
 
 @end
--- Project/Zazo/Vendor/ANODA/ANCategories/NSObject/NSObject+ANUserDefaults.m
+++ Project/Zazo/Vendor/ANODA/ANCategories/NSObject/NSObject+ANUserDefaults.m
@@ -10,30 +10,88 @@
 
 @implementation NSObject (ANUserDefaults)
 
+
+#pragma mark - Update Objects
+
+- (void)an_updateObject:(id)object forKey:(NSString*)key
+{
+    if (!ANIsEmpty(key))
+    {
+        if (object)
+        {
+            [[self an_dataSource] setObject:object forKey:key];
+        }
+        else
+        {
+            [[self an_dataSource] removeObjectForKey:key];
+        }
+        [[self an_dataSource] synchronize];
+    }
+}
+
+- (void)an_updateBool:(BOOL)object forKey:(NSString*)key
+{
+    if (!ANIsEmpty(key))
+    {
+        [[self an_dataSource] setBool:object forKey:key];
+        [[self an_dataSource] synchronize];
+    }
+}
+
+- (void)an_updateInteger:(NSInteger)object forKey:(NSString*)key
+{
+    if (!ANIsEmpty(key))
+    {
+        [[self an_dataSource] setInteger:object forKey:key];
+        [[self an_dataSource] synchronize];
+    }
+}
+
+
+#pragma mark - Retrive Objects
+
+- (id)an_objectForKey:(NSString*)key
+{
+    id object;
+    if (!ANIsEmpty(key))
+    {
+        object = [[self an_dataSource] objectForKey:key];
+    }
+    return object;
+}
+
+- (NSString*)an_stringForKey:(NSString*)key
+{
+    NSString* string = [self an_objectForKey:key];
+    return [NSString stringWithFormat:@"%@", string ? : @""];
+}
+
+- (BOOL)an_boolForKey:(NSString*)key
+{
+    BOOL value;
+    if (!ANIsEmpty(key))
+    {
+        [[self an_dataSource] boolForKey:key];
+    }
+    return value;
+}
+
+- (NSInteger)an_integerForKey:(NSString*)key
+{
+    NSInteger value;
+    if (!ANIsEmpty(key))
+    {
+        [[self an_dataSource] integerForKey:key];
+    }
+    return value;
+}
+
+
 #pragma mark - Private
 
 - (NSUserDefaults *)an_dataSource
 {
     return [NSUserDefaults standardUserDefaults];
-}
-
-- (void)an_updateObject:(id)object forKey:(NSString*)key
-{
-    if (object)
-    {
-        [[self an_dataSource] setObject:object forKey:key];
-    }
-    else
-    {
-        [[self an_dataSource] removeObjectForKey:key];
-    }
-    [[self an_dataSource] synchronize];
-}
-
-- (NSString*)an_stringForKey:(NSString*)key
-{
-    NSString* string = [[self an_dataSource] objectForKey:key];
-    return string ? : @"";
 }
 
 @end
