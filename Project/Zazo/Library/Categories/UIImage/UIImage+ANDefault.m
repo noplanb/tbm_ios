c7e0f69388e87bda7ba0c7b8aaa9bea095715f18
ANODA <dev@anoda.mobi>
2015-09-25 12:24:37 +0300

    launch image ios 9 fix

--- Project/Zazo/Library/Categories/UIImage/UIImage+ANDefault.m
+++ Project/Zazo/Library/Categories/UIImage/UIImage+ANDefault.m
@@ -8,18 +8,10 @@
 
 #import "UIImage+ANDefault.h"
 
-#define IOS9_OR_HIGHER          (9.0 <= SYSTEM_VERSION)
-
 static NSString* const kDefaultIphone6 = @"LaunchImage-800-667h@2x.png";
 static NSString* const kDefaultIphone6Plus = @"LaunchImage-800-Portrait-736h@3x.png";
 static NSString* const kDefaultIphone5 = @"LaunchImage-700-568h";
 static NSString* const kDefaultIphone4 = @"LaunchImage-700";
-
-static NSString* const kDefaultIphone6iOS9 = @"LaunchImageFinal-800-667h@2x.png";
-static NSString* const kDefaultIphone6PlusiOS9 = @"LaunchImageFinal-800-Portrait-736h@3x.png";
-static NSString* const kDefaultIphone5iOS9 = @"LaunchImageFinal-700-568h";
-static NSString* const kDefaultIphone4iOS9 = @"LaunchImageFinal-700";
-
 
 @implementation UIImage (ANDefault)
 
@@ -27,27 +19,6 @@ static NSString* const kDefaultIphone4iOS9 = @"LaunchImageFinal-700";
 {
     UIImage* result;
     
-    if (IOS9_OR_HIGHER)
-    {
-        if (IS_IPHONE_6)
-        {
-            result = [UIImage imageNamed:kDefaultIphone6iOS9];
-        }
-        else if (IS_IPHONE_6_PLUS)
-        {
-            result = [UIImage imageNamed:kDefaultIphone6PlusiOS9];
-        }
-        else if (IS_IPHONE_5)
-        {
-            result = [UIImage imageNamed:kDefaultIphone5iOS9];
-        }
-        else
-        {
-            result = [UIImage imageNamed:kDefaultIphone4iOS9];
-        }
-    }
-    else
-    {
         if (IS_IPHONE_6)
         {
             result = [UIImage imageNamed:kDefaultIphone6];
@@ -64,8 +35,6 @@ static NSString* const kDefaultIphone4iOS9 = @"LaunchImageFinal-700";
         {
             result = [UIImage imageNamed:kDefaultIphone4];
         }
-
-    }
     
     return result;
 }
