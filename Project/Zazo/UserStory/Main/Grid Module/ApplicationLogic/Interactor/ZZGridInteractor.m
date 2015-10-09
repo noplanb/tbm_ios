3f4a920417af6bcc7a033af865b273a0c7fc56bf
ANODA <dev@anoda.mobi>
2015-10-09 21:13:53 +0300

    fix ios7 grid

--- Project/Zazo/UserStory/Main/Grid Module/ApplicationLogic/Interactor/ZZGridInteractor.m
+++ Project/Zazo/UserStory/Main/Grid Module/ApplicationLogic/Interactor/ZZGridInteractor.m
@@ -138,10 +138,7 @@ static NSInteger const kGridFriendsCellCount = 8;
 - (NSArray*)_gridModels
 {
     NSArray* gridModels = [ZZGridDataProvider loadAllGridsSortByIndex:NO];
-    if (gridModels.count != kGridFriendsCellCount)
-    {
-        gridModels = [ZZGridDataProvider loadOrCreateGridModelsWithCount:kGridFriendsCellCount];
-    }
+    gridModels = [ZZGridDataProvider loadOrCreateGridModelsWithCount:kGridFriendsCellCount];
     NSSortDescriptor* sort = [NSSortDescriptor sortDescriptorWithKey:@"indexPathIndexForItem" ascending:YES];
     return [gridModels sortedArrayUsingDescriptors:@[sort]];
 }
