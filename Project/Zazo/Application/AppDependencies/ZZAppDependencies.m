64e3c6b5e1e5f5d5ef06c5016ccc7dc2eced9a44
Maksim Bazarov <bazaroffma@gmail.com>
2015-10-15 02:39:16 +0300

    logger

--- Project/Zazo/Application/AppDependencies/ZZAppDependencies.m
+++ Project/Zazo/Application/AppDependencies/ZZAppDependencies.m
@@ -19,7 +19,13 @@
 #import "ZZCoreTelephonyConstants.h"
 #import "ZZUserDataProvider.h"
 #import "ZZUserDomainModel.h"
+#import "OBLoggerNotification.h"
+#import <Crashlytics/Crashlytics.h>
 
+#warning REMOVE AFTER OBLOGGER UPDATED
+#define OBLoggerEventNotification @"OBLoggerEventNotification"
+#define OBLoggerInfoNotification @"OBLoggerInfoNotification"
+#define OBLoggerDebugNotification @"OBLoggerDebugNotification"
 
 @interface ZZAppDependencies ()
 
@@ -37,6 +43,13 @@
 
 - (void)initialApplicationSetup:(UIApplication *)application launchOptions:(NSDictionary *)options
 {
+    // Setup listening log notifications
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_logEvent:) name:OBLoggerEventNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_logError:) name:OBLoggerErrorNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_logWarn:) name:OBLoggerWarnNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_logInfo:) name:OBLoggerInfoNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_logDebug:) name:OBLoggerDebugNotification object:nil];
+
     [MagicalRecord setLoggingLevel:MagicalRecordLoggingLevelInfo];
     [ZZContentDataAcessor start];
     ANDispatchBlockToBackgroundQueue(^{
@@ -45,6 +58,40 @@
         [ZZColorTheme shared];
         [self _handleIncomingCall];
     });
+}
+
+- (void)_logDebug:(NSNotification*)notification
+{
+    [self _log:@" DEBUG " message:notification];
+}
+
+- (void)_logInfo:(NSNotification*)notification
+{
+    [self _log:@" INFO " message:notification];
+}
+
+- (void)_logWarn:(NSNotification*)notification
+{
+    [self _log:@" WARN " message:notification];
+}
+
+- (void)_logError:(NSNotification*)notification
+{
+    [self _log:@" ERROR " message:notification];
+}
+
+- (void)_logEvent:(NSNotification*)notification
+{
+    [self _log:@" EVENT " message:notification];
+}
+
+- (void)_log:(NSString*)prefix message:(NSNotification*)notification
+{
+    if ([notification.object isKindOfClass:[NSString class]])
+    {
+        NSString* message = notification.object;
+        CLS_LOG(@"[%@] : %@",prefix,message);
+    }
 }
 
 - (void)handleWillResignActive
@@ -112,7 +159,10 @@
 
 
 #pragma mark - Private
-
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 - (ZZRootWireframe *)rootWireframe
 {
     if (!_rootWireframe)
