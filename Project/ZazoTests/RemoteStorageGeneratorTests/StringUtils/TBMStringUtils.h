263a2a7b73b56f869280db6acc5d6b82d88b38db
ANODA <dev@anoda.mobi>
2015-10-27 16:21:54 +0200

    remote storage generator start testing

---
+++ Project/ZazoTests/RemoteStorageGeneratorTests/StringUtils/TBMStringUtils.h
@@ -0,0 +1,16 @@
+//
+//  TBMStringUtils.h
+//  tbm
+//
+//  Created by Sani Elfishawy on 5/24/14.
+//  Copyright (c) 2014 No Plan B. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TBMStringUtils : NSObject
++ (NSString *)randomStringofLength:(int)length;
+
++ (NSString *)jsonWithDictionary:(NSDictionary *)dict;
++ (NSDictionary *)dictionaryWithJson:(NSString *)jsonString;
+@end
