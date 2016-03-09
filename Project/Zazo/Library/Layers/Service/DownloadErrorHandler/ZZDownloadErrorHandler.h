2e09129ab1a6a20dd535efe56b80fb19988294b1
Rinat Gabdullin <lostrinat@gmail.com>
2016-03-09 15:44:27 +0300

    Try again dialog

---
+++ Project/Zazo/Library/Layers/Service/DownloadErrorHandler/ZZDownloadErrorHandler.h
@@ -0,0 +1,34 @@
+//
+// Created by Rinat on 09/03/16.
+// Copyright (c) 2016 No Plan B. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ZZVideoStatusHandler.h"
+
+@class ZZVideoFileHandler;
+
+// Handles permanent download errors
+
+// 1. Observes video state changes when app in foreground
+// 2. Finds failed videos when become foreground
+// 3. Finds failed videos when app launched
+
+// If "state = permanent error" then shows dialog "try again?"
+// If "yes" then "repeats download"
+// If "no" then deletes video (deletion means setting ghost state)
+
+// Both decisions will be applied to all videos currently failed to download
+
+@protocol ZZDownloadErrorHandlerUI
+
+- (void)showRetryConfirmationDialog:(void (^)(BOOL needsRetry))completion;
+
+@end
+
+@interface ZZDownloadErrorHandler : NSObject
+
+- (void)startService;
+@property (nonatomic, strong) ZZVideoFileHandler *videoFileHandler;
+
+@end
