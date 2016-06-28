ce46b122538092e65289743633dad9a5e025bc44
Rinat <lostrinat@gmail.com>
2016-06-28 13:17:15 +0300

    Podfile update

---
+++ Project/Pods/Target Support Files/Pods-ZazoBase-ZazoTests/Pods-ZazoBase-ZazoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZazoBase_ZazoTests : NSObject
+@end
+@implementation PodsDummy_Pods_ZazoBase_ZazoTests
+@end
--- Project/Pods/Target Support Files/Pods-ZazoTests/Pods-ZazoTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_ZazoTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ZazoTestsVersionString[];
-
